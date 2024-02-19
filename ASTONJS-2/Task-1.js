18d74d454162bfb4222e2543bdf475c3e9143522
MolchanovEG <146551641+MolchanovEG@users.noreply.github.com>
2024-02-19 15:01:42 +0300

    ASTONJS-2 [fix]: The first task now takes values, not an array

--- ASTONJS-2/Task-1.js
+++ ASTONJS-2/Task-1.js
@@ -1,24 +1,7 @@
-// test
-let arr = [
-  1,
-  "Hello",
-  true,
-  Symbol(),
-  null,
-  undefined,
-  -5.6,
-  {},
-  function () {
-    console.log("Hello World!");
-  },
-];
-
 function getLength(arr) {
-  arr.forEach((arr) => {
-    if (arr != NaN)
-      if (arr?.length ?? 0) console.log(arr.length);
-      else console.log(0);
-  });
+  if (arr != NaN)
+    if (arr?.length ?? 0) console.log(arr.length);
+    else console.log(0);
 }
 
 getLength(arr);
