e5e6f31c1478864b902c37edd717dbf282e82441
MolchanovEG <146551641+MolchanovEG@users.noreply.github.com>
2024-02-19 15:43:48 +0300

    ASTONJS-2 [fix]: The first task handles the map and set

--- ASTONJS-2/Task-1.js
+++ ASTONJS-2/Task-1.js
@@ -1,6 +1,7 @@
 function getLength(arr) {
   if (arr != NaN)
-    if (arr?.length ?? 0) console.log(arr.length);
+    if ((arr?.length ?? 0) || (arr?.size ?? 0))
+      console.log(arr.length || arr.size);
     else console.log(0);
 }
 
