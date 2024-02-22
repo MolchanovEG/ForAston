0f07d456eaa3ca5803851b60937cec8cad175c8e
MolchanovEG <146551641+MolchanovEG@users.noreply.github.com>
2024-02-22 15:41:40 +0300

    ASTONJS-3 [fix]: The fifth task has been corrected for manual deep comparison

--- ASTONJS-3/Task5.js
+++ ASTONJS-3/Task5.js
@@ -1,12 +1,56 @@
 function deepEqual(obj1, obj2) {
-  console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
+  if (obj1 === obj2) return true;
+
+  if (
+    typeof obj1 !== "object" ||
+    typeof obj2 !== "object" ||
+    obj1 == null ||
+    obj2 == null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(obj1).sort();
+  const keysB = Object.keys(obj2).sort();
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  let result = true;
+
+  keysA.forEach((key) => {
+    if (!keysB.includes(key)) {
+      result = false;
+    }
+
+    if (typeof obj1[key] === "function" || typeof obj2[key] === "function") {
+      if (obj1[key].toString() !== obj2[key].toString()) {
+        result = false;
+      }
+    }
+
+    if (!deepEqual(obj1[key], obj2[key])) {
+      result = false;
+    }
+  });
+
+  return result;
 }
 
 //test
 const Z = { elf: 10 };
 
-const obj1 = { here: { is: "on", other: "3" }, object: Z };
+const obj1 = {
+  a: null,
+  object: Z,
+  here: { is: "on", other: "3" },
+};
 
-const obj2 = { here: { is: "on", other: "2" }, object: Z };
+const obj2 = {
+  here: { is: "on", other: "3" },
+  object: Z,
+  a: null,
+};
 
-deepEqual(obj1, obj2);
+console.log(deepEqual(obj1, obj2));
