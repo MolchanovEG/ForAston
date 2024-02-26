147fe15313d76460d7961a9fc0080881bcb88d13
MolchanovEG <146551641+MolchanovEG@users.noreply.github.com>
2024-02-26 11:26:57 +0300

    ASTONJS-4 [fix]: Add task 4 without call and apply

--- ASTONJS-4/Task3.js
+++ ASTONJS-4/Task3.js
@@ -14,3 +14,25 @@ let obj = {
 
 let newFunc = myFunc.myBind(obj, "random words");
 newFunc("New York");
+
+//////////////////////////
+// without apply and call
+
+const person = {
+  firstName: "Jon",
+  lastName: "Doe",
+};
+
+const fullName = function (city) {
+  console.log(this.firstName + " " + this.lastName + " " + city);
+};
+
+Function.prototype.myBind = function (scope, ...args) {
+  scope._this = this;
+  return function () {
+    return scope._this(...args);
+  };
+};
+
+const name = fullName.myBind(person, "NH");
+name();
